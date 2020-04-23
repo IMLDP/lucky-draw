404c004164d1e6756776d9b348ef95f6129517bb
IMLDP <liudingpo@qq.com>
2020-04-24 05:29:44 +0800

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   // 基本路径
-<<<<<<< HEAD
-  publicPath: '/',
-=======
-  publicPath: process.env.NODE_ENV === "production" ? "/lucky-draw" : "/"
->>>>>>> a55e483f8faeb74ecbdc18ae9d86bc0d62a7ec09
+  publicPath: './',
   // 输出文件目录
   outputDir: 'dist',
   configureWebpack: {
